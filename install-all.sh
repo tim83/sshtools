8eafb4177b1849d469500904ada980cc2b674948
Tim Mees <tim.mees83@gmail.com>
2021-10-10 14:24:38 +0200

    Clean-up files

--- install-all.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-bash ~/Programs/ssh-tools/install.sh
-
-ssync
-sshin serverpi -c "ssync"
-for dev in envy serverpi camerapi desktop toshiba; do
-	sshin $dev -s -c "bash ~/Programs/ssh-tools/install.sh"
-done
