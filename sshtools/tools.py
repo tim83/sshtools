ec19ea319d09c7144f785ac582fc636161b090c6
Tim Mees <tim.mees83@gmail.com>
2021-12-27 18:38:09 +0100

    Enable user config

--- sshtools/tools.py
+++ sshtools/tools.py
@@ -1,4 +1,11 @@
 from pathlib import Path
 
 PROJECT_DIR = Path(__file__).parent
-CONFIG_DIR = PROJECT_DIR.parent / "config"
+
+src_config_dir = PROJECT_DIR.parent / "config"
+user_config_dir = Path.home() / ".config/sshtools"
+CONFIG_DIR: Path
+if user_config_dir.is_dir():
+    CONFIG_DIR = user_config_dir
+else:
+    CONFIG_DIR = src_config_dir
