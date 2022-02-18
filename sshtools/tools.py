5aa1433e411ff468927ac64633d6bd9b19bc25e5
Tim Mees <tim.mees83@gmail.com>
2022-02-19 00:16:49 +0100

    Add docstring

--- sshtools/tools.py
+++ sshtools/tools.py
@@ -24,4 +24,11 @@ IP_CACHE_TIMEOUT: int = 5
 
 
 def mt_filter(func: Callable, collection: Iterable, max_workers=20) -> list:
+    """
+    Filters a list based on a function, by using parallel processing
+    :param func: The function that filters the list (must return a bool)
+    :param collection: The list that needs to be filtered
+    :param max_workers: The number of concurrent items that can be filtered
+    :return: The filtered list
+    """
     return timtools.multithreading.mt_filter(func, collection, max_workers=max_workers)
