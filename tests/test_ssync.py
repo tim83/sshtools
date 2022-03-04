411c86311c2fcdac6678182b156472cfb21250b4
Tim Mees <tim.mees83@gmail.com>
2022-03-04 09:01:58 +0100

    Remove test that required present devices

--- tests/test_ssync.py
+++ tests/test_ssync.py
@@ -1,6 +1,3 @@
-import tempfile
-from pathlib import Path
-
 import sshtools.device
 import sshtools.ssync
 
@@ -29,19 +26,3 @@ def test_relevant_devices():
     master, slaves = sshtools.ssync.get_relevant_devices(None, "thinkcentre", None)
     assert master == sshtools.device.Device("thinkcentre")
     assert slaves == [sshtools.device.Device.get_self()]
-
-
-def test_cmd():
-    ssync = sshtools.ssync.Sync(
-        sshtools.device.Device.get_self(),
-        [sshtools.device.Device("oracle")],
-        dry_run=True,
-        connect=False,
-    )
-    with tempfile.TemporaryDirectory() as tmp_dir_obj:
-        tmp_dir = Path(tmp_dir_obj)
-        cmd = ssync.get_cmd(sshtools.device.Device("oracle"), tmp_dir)
-        assert "--dry-run" in cmd
-        assert cmd[0] == "rsync"
-        assert isinstance(cmd, list)
-        assert any(isinstance(c, str) for c in cmd)
